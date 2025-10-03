29c4c846f9bd8f226827155564ffafab8633181d
Иван Мулявин <Tyuiu.MuliavinIM@outlook.com>
2025-10-03 22:20:15 +0500

    Task2

---
+++ Tyuiu.MuliavinIM.Sprint1.Task2V4.Lib/DataService.cs
@@ -0,0 +1,13 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+
+namespace Tyuiu.MuliavinIM.Sprint1.Task2.V4.Lib
+
+{
+    public class DataService : ISprint1Task2V4
+    {
+        public int CalculateSquare(int value)
+        {
+            return value * value;
+        }
+    }
+}
---
+++ Tyuiu.MuliavinIM.Sprint1.Task2V4/Program.cs
@@ -0,0 +1,41 @@
+﻿using Tyuiu.MuliavinIM.Sprint1.Task2.V4.Lib;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #1 | Выполнил: Мулявин И. М. | ИСТНб-25-1";
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #1                                                               *");
+        Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+        Console.WriteLine("* Задание #2                                                              *");
+        Console.WriteLine("* Вариант #4                                                              *");
+        Console.WriteLine("* Выполнил: Мулявин Иван Михайлович | ИСТНб-25-1                          *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Написать программу, которая запрашивает у пользователя                  *");
+        Console.WriteLine("* исходные данные, выполняет указанные расчёты и печатает                 *");
+        Console.WriteLine("* результат на экране.                                                    *");
+        Console.WriteLine("* Формулировка задания: Известна длина стороны квадрата.                  *");
+        Console.WriteLine("* Вычислить площадь квадрата.                                             *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        int x;
+
+        Console.WriteLine("Введите длину стороны квадрата:");
+        x = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine("Площадь квадрата = " + ds.CalculateSquare(x));
+
+        Console.ReadLine();
+    }
+}

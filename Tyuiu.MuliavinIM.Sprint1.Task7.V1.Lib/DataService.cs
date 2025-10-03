4b1f6c2d88c94b1ae6a0b3a50d9776e14571de6d
Иван Мулявин <Tyuiu.MuliavinIM@outlook.com>
2025-10-04 00:50:47 +0500

    Task7

---
+++ Tyuiu.MuliavinIM.Sprint1.Task7.V1.Lib/DataService.cs
@@ -0,0 +1,20 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+
+namespace Tyuiu.MuliavinIM.Sprint1.Task7.V1.Lib
+{
+    public class DataService : ISprint1Task7V1
+    {
+        public double Calculate(double a, double b, double c)
+        {
+            double discriminant = b * b + 4 * a * c;
+            double numerator = b + Math.Sqrt(discriminant);
+            double denominator = 2 * a;
+            double p1 = numerator / denominator;
+            double p2 = Math.Pow(a, 3) * c;
+            double p3 = Math.Pow(b, -2);
+
+            double result = p1 - p2 + p3;
+            return Math.Round(result, 3);
+        }
+    }
+}
---
+++ Tyuiu.MuliavinIM.Sprint1.Task7.V1/Program.cs
@@ -0,0 +1,52 @@
+﻿using Tyuiu.MuliavinIM.Sprint1.Task7.V1.Lib;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #1 | Выполнил: Мулявин И. М. | ИСТНб-25-1";
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #1                                                               *");
+        Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+        Console.WriteLine("* Задание #7                                                              *");
+        Console.WriteLine("* Вариант #1                                                               *");
+        Console.WriteLine("* Выполнил: Мулявин Иван Михайлович | ИСТНб-25-1                          *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Написать программу, которая вычисляет математическое выражение по       *");
+        Console.WriteLine("* исходным значениям данных, вводимых пользователем. Ответ округлите до   *");
+        Console.WriteLine("* 3 знаков после запятой.                                                 *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("*             (b + sqrt(b^2 + 4ac))                                        *");
+        Console.WriteLine(" Формула: z = ---------------------   - a^3 * c + b^(-2)                    ");
+        Console.WriteLine("*                     (2a)                                                *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        double a, b, c;
+
+        Console.WriteLine("Введите значение A:");
+        a = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Введите значение B:");
+        b = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Введите значение C:");
+        c = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+
+        Console.WriteLine(ds.Calculate(a, b, c));
+
+
+        Console.ReadLine();
+    }
+}

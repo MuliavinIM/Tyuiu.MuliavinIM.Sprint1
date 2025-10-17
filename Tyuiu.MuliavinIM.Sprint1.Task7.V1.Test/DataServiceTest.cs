eb9874e5e5eb0af8378302f8858ff38634f01cc7
Иван Мулявин <Tyuiu.MuliavinIM@outlook.com>
2025-10-17 21:09:51 +0500

    Task7

--- Tyuiu.MuliavinIM.Sprint1.Task7.V1.Test/DataServiceTest.cs
+++ Tyuiu.MuliavinIM.Sprint1.Task7.V1.Test/DataServiceTest.cs
@@ -1,4 +1,6 @@
-﻿namespace Tyuiu.MuliavinIM.Sprint1.Task7.V1.Test
+﻿using Tyuiu.MuliavinIM.Sprint1.Task7.V1.Lib;
+
+namespace Tyuiu.MuliavinIM.Sprint1.Task7.V1.Test
 {
     [TestClass]
     public sealed class DataServiceTest
@@ -6,6 +8,13 @@
         [TestMethod]
         public void TestMethod1()
         {
+            DataService ds = new DataService();
+            double a = 1;
+            double b = 2;
+            double c = 3;
+            double wait = 0.25;
+            double res = ds.Calculate(a, b, c);
+            Assert.AreEqual(wait, res);
         }
     }
 }
